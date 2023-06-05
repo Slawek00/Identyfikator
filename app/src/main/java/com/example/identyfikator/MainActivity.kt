e558ea83ec7aa36e0dc634408f91eddb7d4a76fd
slawek00 <slawekjalojda@gmail.com>
2023-06-05 15:28:37 +0200

    Wyłączenie ciemnego motywu

--- app/src/main/java/com/example/identyfikator/MainActivity.kt
+++ app/src/main/java/com/example/identyfikator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -17,10 +18,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Wyłączenie ciemnego trybu
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        //Ominięcie wyjątków z siecią
         if (Build.VERSION.SDK_INT > 9) {
             val policy = ThreadPolicy.Builder().permitAll().build()
             StrictMode.setThreadPolicy(policy)
         }
+        //Databinding i nawigacja
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
